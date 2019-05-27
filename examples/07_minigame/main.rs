bb8bd1f56cc934b42054c6fec76b29206f436dc0
glaceef <masaki0708_art@yahoo.co.jp>
2019-05-27 18:12:04 +0900

    complete 06_animation

--- examples/07_minigame/main.rs
+++ examples/07_minigame/main.rs
@@ -10,11 +10,14 @@ use amethyst::{
         DisplayConfig, RenderBundle,
         SpriteRender
     },
-    input::is_key_down,
+    input::{
+        InputBundle, InputHandler,
+        is_key_down,
+    },
     utils::application_root_dir,
     ecs::prelude::{
-        Entity,
-        System, WriteStorage, ReadExpect
+        Component, DenseVecStorage, Entity,
+        System, ReadStorage, WriteStorage, Read, ReadExpect
     },
     winit::VirtualKeyCode,
 };
@@ -27,7 +30,26 @@ use amethyst_test::{
 
 use std::path::PathBuf;
 
-struct CharaEntity(Entity);
+enum State {
+    Idle, Right, Left
+}
+impl State {
+    fn offset(&self) -> usize {
+        match self {
+            State::Idle => 0,
+            State::Right => 4,
+            State::Left => 8,
+        }
+    }
+}
+
+struct Player(State);
+
+impl Component for Player {
+    type Storage = DenseVecStorage<Self>;
+}
+
+struct PlayerEntity(Entity);
 
 struct ExampleState;
 
@@ -35,7 +57,7 @@ impl SimpleState for ExampleState {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
         initialise_camera(world, [500.0, 500.0]);
-        initialise_character(world);
+        initialise_player(world);
     }
 
     fn handle_event(
@@ -52,18 +74,19 @@ impl SimpleState for ExampleState {
     }
 }
 
-struct CharaSpriteSystem(usize);
+struct PlayerSpriteSystem(usize);
 
-impl<'s> System<'s> for CharaSpriteSystem {
+impl<'s> System<'s> for PlayerSpriteSystem {
     type SystemData = (
+        ReadStorage<'s, Player>,
         WriteStorage<'s, SpriteRender>,
-        ReadExpect<'s, CharaEntity>
+        ReadExpect<'s, PlayerEntity>
     );
 
-    fn run(&mut self, (mut sprites, player): Self::SystemData) {
-        match sprites.get_mut(player.0) {
-            Some(sprite) => {
-                sprite.sprite_number = num_extend(self.0, 4, 16);
+    fn run(&mut self, (players, mut sprites, player): Self::SystemData) {
+        match (players.get(player.0), sprites.get_mut(player.0)) {
+            (Some(player), Some(sprite)) => {
+                sprite.sprite_number = num_extend(self.0, 4, 7) + player.0.offset();
                 self.0 += 1;
             }
             _ => {}
@@ -71,6 +94,35 @@ impl<'s> System<'s> for CharaSpriteSystem {
 
         fn num_extend(n: usize, size: usize, repeat: usize) -> usize {
             n % (size * repeat) / repeat
+        }
+    }
+}
+
+struct PlayerMoveSystem;
+
+impl<'s> System<'s> for PlayerMoveSystem {
+    type SystemData = (
+        WriteStorage<'s, Player>,
+        WriteStorage<'s, Transform>,
+        ReadExpect<'s, PlayerEntity>,
+        Read<'s, InputHandler<String, String>>
+    );
+
+    fn run(&mut self, (mut players, mut transforms, player, input): Self::SystemData) {
+        let dx = input.axis_value("x_axis").unwrap() as f32;
+        let dy = input.axis_value("y_axis").unwrap() as f32;
+        let speed = 3.0;
+        if let Some(transform) = transforms.get_mut(player.0) {
+            transform.translate_xyz(dx * speed, dy * speed, 0.0);
+        }
+
+        if let Some(player) = players.get_mut(player.0) {
+            player.0 = match dx as i32 {
+                 0 => State::Idle,
+                 1 => State::Right,
+                -1 => State::Left,
+                _ => panic!(),
+            };
         }
     }
 }
@@ -94,30 +146,34 @@ fn main() -> amethyst::Result<()> {
 
     let transform_bundle = TransformBundle::new();
 
+    let input_bundle = InputBundle::<String, String>::new()
+        .with_bindings_from_file(app_root.join("bindings.ron"))?;
+
     let game_data = GameDataBuilder::new()
         .with_bundle(render_bundle.with_sprite_sheet_processor())?
         .with_bundle(transform_bundle)?
-        .with(CharaSpriteSystem(0), "chara_sprite_system", &[]);
+        .with_bundle(input_bundle)?
+        .with(PlayerSpriteSystem(0), "player_sprite_system", &[])
+        .with(PlayerMoveSystem, "player-move-system", &[]);
 
     Application::new(app_root, ExampleState, game_data)?.run();
 
     Ok(())
 }
 
-fn initialise_character(world: &mut World) {
+fn initialise_player(world: &mut World) {
+    world.register::<Player>();
     let sprite_sheet = load_sprite_sheet(world, "dot_reimu.png", "spritesheet.ron");
     let sprite_render = SpriteRender {
         sprite_sheet: sprite_sheet,
         sprite_number: 0,
     };
-
-    let scale = 2.0;
-    let mut transform = Transform::from_xyz(250.0, 250.0, 0.0);
-        transform.set_scale(scale, scale, 1.0);
+    let transform = Transform::from_xyz(250.0, 250.0, 0.0);
 
     let entity = world.create_entity()
+        .with(Player(State::Idle))
         .with(sprite_render)
         .with(transform)
         .build();
-    world.add_resource(CharaEntity(entity));
+    world.add_resource(PlayerEntity(entity));
 }
